fb3f6a8235b9dd72f276506f417c1af5b1f3421f
KD <48119415+kumardulal@users.noreply.github.com>
2021-10-11 03:57:12 +0800

    connection bb changed

--- Connectiondb.js
+++ Connectiondb.js
@@ -1,9 +1,10 @@
 const mysql = require('mysql')
 
 const dbConnection = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "",
+    host: "aamgrko7373xg.cphznuprlsbn.us-east-2.rds.amazonaws.com",
+    user: "nltserveruser",
+    password: "nltserver",
     database: "newleaderstechwebsite",
+    connectTimeout: "20000"
 });
 module.exports = dbConnection;
